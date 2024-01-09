a860c8c437dbd9e7c3198df52793de7cb51d51c5
danielreinhard1129 <119338658+danielreinhard1129@users.noreply.github.com>
2024-01-09 13:25:34 +0700

    Initial commit

---
+++ apps/api/src/app.ts
@@ -0,0 +1,68 @@
+import express, {
+  json,
+  urlencoded,
+  Express,
+  Request,
+  Response,
+  NextFunction,
+  Router,
+} from 'express';
+import cors from 'cors';
+import { PORT } from './config';
+import { SampleRouter } from './routers/sample.router';
+
+export default class App {
+  private app: Express;
+
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.routes();
+    this.handleError();
+  }
+
+  private configure(): void {
+    this.app.use(cors());
+    this.app.use(json());
+    this.app.use(urlencoded({ extended: true }));
+  }
+
+  private handleError(): void {
+    // not found
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      if (req.path.includes('/api/')) {
+        res.status(404).send('Not found !');
+      } else {
+        next();
+      }
+    });
+
+    // error
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (req.path.includes('/api/')) {
+          console.error('Error : ', err.stack);
+          res.status(500).send('Error !');
+        } else {
+          next();
+        }
+      },
+    );
+  }
+
+  private routes(): void {
+    const sampleRouter = new SampleRouter();
+
+    this.app.get('/', (req: Request, res: Response) => {
+      res.send(`Hello, Purwadhika Student !`);
+    });
+
+    this.app.use('/samples', sampleRouter.getRouter());
+  }
+
+  public start(): void {
+    this.app.listen(PORT, () => {
+      console.log(`  ➜  [API] Local:   http://localhost:${PORT}/`);
+    });
+  }
+}
